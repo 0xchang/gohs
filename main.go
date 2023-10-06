63aeeec536daa2af252dfdf0163348a8b13a3a3f
oxchang <oxchang@163.com>
2023-10-06 13:43:33 +0800

    src

--- main.go
+++ main.go
@@ -8,19 +8,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var port int
+var addr string
+var root string
+
 func main() {
 
-	var port int
-	var addr string
-	var root string
+	flag.IntVar(&port, "p", 8075, "set port")
+	flag.StringVar(&addr, "a", "0.0.0.0", "set ip addr")
+	flag.StringVar(&root, "r", ".", "set root path")
 
-	flag.IntVar(&port, "p", 8075, "as")
-	flag.StringVar(&addr, "a", "0.0.0.0", "asd")
-	flag.StringVar(&root, "r", ".", "")
+	flag.Parse()
 
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
+
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Cache-Control", "no-store")
+		c.Next()
+	})
 
 	// 设置静态文件路由
 	router.StaticFS("/", http.Dir(root))
